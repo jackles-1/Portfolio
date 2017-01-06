538cd05e72e34cc5dfcde641994dbc35c44778f0
Jackie Stonebraker <sunktanka@gmail.com>
2017-01-05 17:56:30 -0700

    add file structure; experiment with arrow fading

---
+++ assets/js/home.js
@@ -0,0 +1,8 @@
+var arrowOffset = $("#arrow").offset();
+var arrowPosition = $("#arrow").position();
+
+var bodyOffset = $("body").offset();
+var bodyPosition = $("body").position();
+
+// continue experimenting with this:
+// http://stackoverflow.com/questions/26694385/fade-in-on-scroll-down-fade-out-on-scroll-up-based-on-element-position-in-win
