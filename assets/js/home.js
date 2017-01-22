50593f1245bcf22aa8b5edccd1e430a1fbc83973
Jackie Stonebraker <sunktanka@gmail.com>
2017-01-22 15:26:32 -0700

    complete home page navigation hover effects; remove jQuery from home page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,19 +1,85 @@
-// CONVERT AWAY FROM jQUERY
-
 // Fade Out Arrow Upon Scrolling Down
-var windowHeight = $(window).height()/3;
+var windowHeight = (window.innerHeight)/3
 var scrollPosition;
 var percentScrolled;
+var arrow = document.querySelector("#arrow");
+var body = document.querySelector("body");
 
-$(window).on("scroll", function(){
-	scrollPosition = $(window).scrollTop();
+window.onscroll = function(){
+	scrollPosition = body.scrollTop;
 	percentScrolled = scrollPosition/windowHeight;
-
+	
 	if(percentScrolled <= 1){
-		 $("#arrow").fadeTo(0, 1-percentScrolled);
+		var percentFade = 1-percentScrolled;
+		arrow.style.opacity = percentFade;
 	}
 	else{
-		$("#arrow").fadeOut(100);
+		arrow.style.opacity = "0";
 	}
-});
+};
 
+// Add Gradient and Text Over Page Icons On Hover and Focus
+var pageIcons = document.querySelectorAll(".circle a");
+
+for(var i=0; i < pageIcons.length; i++){
+	pageIcons[i].addEventListener("mouseover", function(){
+		if(this.getAttribute("class")==="bioPic")
+		{
+			this.classList.add("bioPicHover");
+			this.classList.remove("bioPic");
+			this.textContent = "Bio";
+		}
+		else if(this.getAttribute("class")==="portfolioPic"){
+			this.classList.add("portfolioPicHover");
+			this.classList.remove("portfolioPic");
+			this.textContent = "Portfolio";	
+		}
+		else if(this.getAttribute("class")==="resumePic"){
+			this.classList.add("resumePicHover");
+			this.classList.remove("resumePic");
+			this.textContent = "Resume";	
+		}
+		else if(this.getAttribute("class")==="contactPic"){
+			this.classList.add("contactPicHover");
+			this.classList.remove("contactPic");
+			this.textContent = "Contact!";	
+		}
+	});	
+	pageIcons[i].addEventListener("mouseout", function(){
+		if(this.getAttribute("class")==="bioPicHover")
+		{
+			this.classList.add("bioPic");
+			this.classList.remove("bioPicHover");
+			this.textContent = "";
+		}
+		else if(this.getAttribute("class")==="portfolioPicHover"){
+			this.classList.add("portfolioPic");
+			this.classList.remove("portfolioPicHover");
+			this.textContent = "";
+		}
+		else if(this.getAttribute("class")==="resumePicHover"){
+			this.classList.add("resumePic");
+			this.classList.remove("resumePicHover");
+			this.textContent = "";
+		}
+		else if(this.getAttribute("class")==="contactPicHover"){
+			this.classList.add("contactPic");
+			this.classList.remove("contactPicHover");
+			this.textContent = "";
+		}
+	});
+}
+
+// OLD ARROW FADING WITH jQUERY
+// var windowHeight = $(window).height()/3;
+// $(window).on("scroll", function(){
+// 	scrollPosition = $(window).scrollTop();
+// 	percentScrolled = scrollPosition/windowHeight;
+
+// 	if(percentScrolled <= 1){
+// 		 $("#arrow").fadeTo(0, 1-percentScrolled);
+// 	}
+// 	else{
+// 		$("#arrow").fadeOut(100);
+// 	}
+// });
